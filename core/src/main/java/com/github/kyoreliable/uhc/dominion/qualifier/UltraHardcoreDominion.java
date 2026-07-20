446c0f360473e3797c2b08624fbb51eec48ab43e
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 01:16:53 -0400

    chore: optimize imports in UltraHardcoreDominion annotation

--- core/src/main/java/com/github/kyoreliable/uhc/dominion/qualifier/UltraHardcoreDominion.java
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/qualifier/UltraHardcoreDominion.java
@@ -1,8 +1,15 @@
 package com.github.kyoreliable.uhc.dominion.qualifier;
 
-import java.lang.annotation.*;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Target({FIELD, PARAMETER, METHOD})
 public @interface UltraHardcoreDominion {}
