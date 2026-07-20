4c2290bcc2abbfefc3f1635da8b9a401a6cfdfa4
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 01:25:45 -0400

    feat: integrate Dominion ECS base

--- core/src/main/java/com/github/kyoreliable/uhc/dominion/DominionGuiceModule.java
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/DominionGuiceModule.java
@@ -1,5 +1,16 @@
 package com.github.kyoreliable.uhc.dominion;
 
+import com.github.kyoreliable.uhc.UltraHardcore;
+import com.github.kyoreliable.uhc.dominion.qualifier.UltraHardcoreDominion;
 import com.google.inject.AbstractModule;
+import dev.dominion.ecs.api.Dominion;
 
-public final class DominionGuiceModule extends AbstractModule {}
+public final class DominionGuiceModule extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    this.bind(Dominion.class)
+        .annotatedWith(UltraHardcoreDominion.class)
+        .toInstance(Dominion.create(UltraHardcore.NAMESPACE));
+  }
+}
--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.github.kyoreliable.uhc {
   requires com.google.guice;
   requires org.jspecify;
+  requires dev.dominion.ecs.api;
 }
