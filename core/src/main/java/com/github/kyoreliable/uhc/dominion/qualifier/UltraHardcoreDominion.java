4eec3f47d76eed33309c9225d77e31c9d161913d
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 01:15:14 -0400

    feat: add UltraHardcoreDominion qualifier

---
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/qualifier/UltraHardcoreDominion.java
@@ -0,0 +1,8 @@
+package com.github.kyoreliable.uhc.dominion.qualifier;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+public @interface UltraHardcoreDominion {}
