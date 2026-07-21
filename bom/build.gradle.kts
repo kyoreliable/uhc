0d09f987cab14f10761e8df93ef40ca5137a1d55
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-21 16:58:47 -0400

    chore: configure maven-publish plugin in bom build script

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
   `java-platform`
+  `maven-publish`
 }
 
 dependencies {
   constraints {
     sequenceOf(":core", ":extension:adventure", ":extension:command:cloud").forEach {
       api(project(it))
+    }
+  }
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["javaPlatform"])
     }
   }
 }
