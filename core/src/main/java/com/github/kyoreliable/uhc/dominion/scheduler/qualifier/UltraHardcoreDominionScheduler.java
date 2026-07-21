a374199afa023c2ca9e48d8a3853c9748fe37fde
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 20:09:36 -0400

    feat: add UltraHardcoreDominionScheduler qualifier

---
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/qualifier/UltraHardcoreDominionScheduler.java
@@ -0,0 +1,14 @@
+package com.github.kyoreliable.uhc.dominion.scheduler.qualifier;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({FIELD, PARAMETER})
+public @interface UltraHardcoreDominionScheduler {}
