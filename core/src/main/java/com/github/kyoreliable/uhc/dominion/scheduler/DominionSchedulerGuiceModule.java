8125f205caec7e2a1962325f4cb6367ba30b280c
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 20:09:42 -0400

    feat: add DominionSchedulerGuiceModule with Scheduler provisioning

---
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/DominionSchedulerGuiceModule.java
@@ -0,0 +1,17 @@
+package com.github.kyoreliable.uhc.dominion.scheduler;
+
+import com.github.kyoreliable.uhc.dominion.qualifier.UltraHardcoreDominion;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import dev.dominion.ecs.api.Dominion;
+import dev.dominion.ecs.api.Scheduler;
+
+public final class DominionSchedulerGuiceModule extends AbstractModule {
+
+  @Provides
+  @Singleton
+  public Scheduler provideScheduler(final @UltraHardcoreDominion Dominion dominion) {
+    return dominion.createScheduler();
+  }
+}
---
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.github.kyoreliable.uhc.dominion.scheduler;
+
+import org.jspecify.annotations.NullMarked;
