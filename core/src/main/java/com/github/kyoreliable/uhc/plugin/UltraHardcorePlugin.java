018d4a95b371f8ffb77d64b514a5d69b4ea517dd
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-21 18:55:27 -0400

    chore: move UltraHardcorePlugin to dedicated plugin package and add package-info.java

--- core/src/main/java/com/github/kyoreliable/uhc/plugin/UltraHardcorePlugin.java
+++ core/src/main/java/com/github/kyoreliable/uhc/plugin/UltraHardcorePlugin.java
@@ -1,4 +1,4 @@
-package com.github.kyoreliable.uhc;
+package com.github.kyoreliable.uhc.plugin;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
