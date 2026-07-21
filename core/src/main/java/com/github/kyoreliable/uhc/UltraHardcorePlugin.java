3df18dbe44cd39ac212fa07ef7d88c0050dbb247
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 23:24:53 -0400

    feat: add @UltraHardcorePlugin qualifier

---
+++ core/src/main/java/com/github/kyoreliable/uhc/UltraHardcorePlugin.java
@@ -0,0 +1,16 @@
+package com.github.kyoreliable.uhc;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import jakarta.inject.Qualifier;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({FIELD, PARAMETER})
+public @interface UltraHardcorePlugin {}
