725c1c4771e6c3521ba86bbf6c1ad8099719676e
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 23:24:43 -0400

    chore: apply @NullMarked to UltraHardcore class

--- core/src/main/java/com/github/kyoreliable/uhc/UltraHardcore.java
+++ core/src/main/java/com/github/kyoreliable/uhc/UltraHardcore.java
@@ -1,5 +1,8 @@
 package com.github.kyoreliable.uhc;
 
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 public final class UltraHardcore {
 
   public static final String NAMESPACE = "uhc";
