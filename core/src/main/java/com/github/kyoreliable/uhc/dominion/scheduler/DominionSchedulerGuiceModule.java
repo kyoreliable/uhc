ce2e7ce97f4004e056a2cf423292f6b112fa15c8
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-20 20:38:30 -0400

    chore: apply @NullMarked to DominionSchedulerGuiceModule and remove it from package-info.java as only that class needs it

--- core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/DominionSchedulerGuiceModule.java
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/DominionSchedulerGuiceModule.java
@@ -6,7 +6,9 @@ import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import dev.dominion.ecs.api.Dominion;
 import dev.dominion.ecs.api.Scheduler;
+import org.jspecify.annotations.NullMarked;
 
+@NullMarked
 public final class DominionSchedulerGuiceModule extends AbstractModule {
 
   @Provides
--- core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/package-info.java
+++ core/src/main/java/com/github/kyoreliable/uhc/dominion/scheduler/package-info.java
@@ -1,4 +1,1 @@
-@NullMarked
 package com.github.kyoreliable.uhc.dominion.scheduler;
-
-import org.jspecify.annotations.NullMarked;
