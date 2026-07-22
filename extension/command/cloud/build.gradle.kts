a2f7f44021adc54cd64d8ffdf3966b8db6e06104
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-21 21:47:05 -0400

    chore: remove cloud command extension

--- extension/command/cloud/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-  com.github.kyoreliable.uhc.`extension-conventions`
-}
-
-dependencies {
-  api(platform(libs.cloud.bom))
-  compileOnlyApi(libs.bundles.cloud)
-}
--- extension/command/cloud/src/main/java/com/github/kyoreliable/uhc/cloud/CloudGuiceModule.java
+++
@@ -1,5 +0,0 @@
-package com.github.kyoreliable.uhc.cloud;
-
-import com.google.inject.AbstractModule;
-
-public final class CloudGuiceModule extends AbstractModule {}
