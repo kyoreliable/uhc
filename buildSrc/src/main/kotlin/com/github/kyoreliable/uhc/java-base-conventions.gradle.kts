b2fac193f2f1dbd7f05fbc7ee6e20e3f75db05ab
kyoreliable <kiwizz.professionel@gmail.com>
2026-07-21 21:54:06 -0400

    chore: remove buildSrc module and related convention scripts

--- buildSrc/src/main/kotlin/com/github/kyoreliable/uhc/java-base-conventions.gradle.kts
+++
@@ -1,9 +0,0 @@
-package com.github.kyoreliable.uhc
-
-plugins.withType<JavaPlugin> {
-  extensions.getByType<JavaPluginExtension>().apply {
-    toolchain {
-      languageVersion = JavaLanguageVersion.of(25)
-    }
-  }
-}
